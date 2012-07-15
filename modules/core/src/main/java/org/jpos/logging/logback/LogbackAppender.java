a60bc4abb3fc981be3f6290b4be82044b8bb33c9
Victor Salaman <vsalaman@vmantek.com>
2012-07-15 09:57:25 -0400

    Import logback support

---
+++ modules/core/src/main/java/org/jpos/logging/logback/LogbackAppender.java
@@ -0,0 +1,69 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2012 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.logging.logback;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.ThrowableProxyUtil;
+import ch.qos.logback.core.AppenderBase;
+import org.jpos.util.Log;
+import org.jpos.util.LogEvent;
+import org.jpos.util.Logger;
+
+/**
+ * Logback Appender.
+ *
+ * @author Victor Salaman
+ */
+public class LogbackAppender extends AppenderBase<ILoggingEvent>
+{
+    Logger logger;
+
+    @Override
+    public void start()
+    {
+        logger = Logger.getLogger(getName());
+        super.start();
+    }
+
+    @Override
+    protected void append(final ILoggingEvent event)
+    {
+        if (!isStarted())
+        {
+            return;
+        }
+
+        // Build
+        final Log source = new Log(logger, getName()+"/"+event.getLoggerName());
+        final LogEvent ev = new LogEvent(source,
+                                         event.getLevel().levelStr.toLowerCase(),
+                                         event.getFormattedMessage());
+
+        // Handle exceptions
+        IThrowableProxy tp = event.getThrowableProxy();
+        if (tp != null)
+        {
+            ev.addMessage(ThrowableProxyUtil.asString(tp));
+        }
+
+        // Do the actual logging
+        Logger.log(ev);
+    }
+}
---
+++ modules/core/src/main/java/org/jpos/logging/logback/LogbackConfigurator.java
@@ -0,0 +1,111 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2012 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.logging.logback;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.status.ErrorStatus;
+import ch.qos.logback.core.status.Status;
+import ch.qos.logback.core.status.StatusChecker;
+import ch.qos.logback.core.status.StatusManager;
+import ch.qos.logback.core.status.StatusUtil;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.q2.QBeanSupport;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Logback Configurator.
+ *
+ * @author Victor Salaman
+ */
+public class LogbackConfigurator extends QBeanSupport
+{
+    private String configResource;
+
+    @Override
+    public void setConfiguration(Configuration cfg) throws ConfigurationException
+    {
+        super.setConfiguration(cfg);
+        configResource = cfg.get("config-resource", "classpath:/logback.xml");
+    }
+
+    @Override
+    protected void startService() throws Exception
+    {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        try
+        {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            context.reset();
+
+            if (configResource.startsWith("file:"))
+            {
+                final String f = configResource.substring(5);
+                log.info("Reading logback configuration from file: "+f);
+                configurator.doConfigure(new File(f));
+            }
+            else if (configResource.startsWith("classpath:"))
+            {
+                final URL r = getClass().getResource(configResource.substring(10));
+                log.info("Reading logback configuration from classpath resource: "+r.toString());
+                configurator.doConfigure(r);
+            }
+        }
+        catch (JoranException je)
+        {
+            log.error("Logger configuration exception", je);
+        }
+
+        printStatusIfWarningOrError(context);
+    }
+
+    private void printStatusIfWarningOrError(LoggerContext context)
+    {
+        StatusChecker sc = new StatusChecker(context);
+        if (sc.getHighestLevel(0) >= ErrorStatus.WARN)
+        {
+            printStatus(context);
+        }
+    }
+
+    private void printStatus(LoggerContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        StatusManager sm = context.getStatusManager();
+        List<Status> statusList = StatusUtil.filterStatusListByTimeThreshold(sm.getCopyOfStatusList(), 0);
+        if (statusList != null)
+        {
+            for (Status s : statusList)
+            {
+                StatusPrinter.buildStr(sb, "", s);
+            }
+        }
+
+        log.error(sb.toString());
+    }
+}

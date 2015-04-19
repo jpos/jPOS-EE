5da2005ffd73cec43bf0b3091b74359e2178c26d
Alejandro Revilla <apr@jpos.org>
2015-04-19 11:03:35 -0300

    Use PBKDF2WithHmacSHA256 to store passwords
    with backward compatibility

--- modules/eeuser/src/main/java/org/jpos/ee/UserManager.java
+++ modules/eeuser/src/main/java/org/jpos/ee/UserManager.java
@@ -18,39 +18,61 @@
 
 package org.jpos.ee;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.List;
+
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.encoders.Base64;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.ObjectNotFoundException;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.Session;
 import org.jpos.iso.ISOUtil;
+import org.jpos.security.SystemSeed;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 /**
  * @author Alejandro Revilla
  */
-@SuppressWarnings("unused")
 public class UserManager {
     DB db;
-    Session session;
-    String digest;
-
+    VERSION version;
     public UserManager (DB db) {
         super ();
         this.db = db;
-        this.session = db.session();
+        version = VERSION.ZERO;
     }
-    public UserManager (Session session) {
+
+    public UserManager (DB db, VERSION version) {
         super ();
-        this.session = session;
+        this.db = db;
+        this.version = version;
+    }
+
+    /**
+     * @return all users
+     * @throws HibernateException on low level hibernate related exception
+     */
+    public List findAll () throws HibernateException {
+        return db.session().createCriteria (User.class)
+                .add (Restrictions.eq ("deleted", Boolean.FALSE))
+                .list();
+    }
+    public User getUserByNick (String nick)
+            throws HibernateException
+    {
+        return getUserByNick(nick, false);
     }
     public User getUserByNick (String nick, boolean includeDeleted)
         throws HibernateException
     {
         try {
-            Criteria crit = session.createCriteria (User.class)
+            Criteria crit = db.session().createCriteria (User.class)
                 .add (Restrictions.eq ("nick", nick));
             if (!includeDeleted)
                 crit = crit.add (Restrictions.eq ("deleted", Boolean.FALSE));
@@ -58,27 +80,7 @@ public class UserManager {
         } catch (ObjectNotFoundException ignored) { }
         return null;
     }
-    public User getUserByNick (String nick)
-        throws HibernateException
-    {
-        return getUserByNick (nick, false);
-    }
-    /**
-     * @param nick name.
-     * @param seed initial seed
-     * @param pass hash
-     * @return the user
-     * @throws BLException if invalid user/pass
-     * @throws HibernateException on low level hibernate related exception
-     */
-    public User getUserByNick (String nick, String seed, String pass)
-        throws HibernateException, BLException
-    {
-        User u = getUserByNick (nick);
-        assertNotNull (u, "User does not exist");
-        assertTrue (checkPassword (u, seed, pass), "Invalid password");
-        return u;
-    }
+
     /**
      * @param nick name.
      * @param pass hash
@@ -89,51 +91,69 @@ public class UserManager {
     public User getUserByNick (String nick, String pass)
         throws HibernateException, BLException
     {
-        User u = getUserByNick (nick);
+        User u = getUserByNick(nick);
         assertNotNull (u, "User does not exist");
-        assertTrue (checkPassword (u, pass), "Invalid password");
+        assertTrue(checkPassword(u, pass), "Invalid password");
         return u;
     }
-    /**
-     * @param u the user
-     * @param seed initial seed
-     * @param pass hash
-     * @return true if password matches
-     * @throws BLException if invalid user/pass
-     * @throws HibernateException on low level hibernate related exception
-     */
-    public boolean checkPassword (User u, String seed, String pass)
-        throws HibernateException, BLException
-    {
-        assertNotNull (seed, "Invalid seed");
-        assertNotNull (pass, "Invalid pass");
-        String password = u.getPassword();
-        assertNotNull (password, "Password is null");
-        String computedPass = getHash (seed + password);
-        return pass.equals (computedPass);
-    }
-    /**
-     * @param u the user
-     * @param clearpass password in clear
-     * @return true if password matches
-     * @throws BLException if invalid user/pass
-     * @throws HibernateException on low level hibernate related exception
-     */
+
     public boolean checkPassword (User u, String clearpass)
-        throws HibernateException, BLException
+            throws HibernateException, BLException
     {
-        assertNotNull (clearpass, "Invalid pass");
-        String password = u.getPassword();
-        assertNotNull (password, "Password is null");
-        return password.equals (getHash(u.getId(), clearpass));
+        assertNotNull(clearpass, "Invalid pass");
+        String passwordHash = u.getPassword();
+        assertNotNull(passwordHash, "Password is null");
+        VERSION v = VERSION.getVersion(passwordHash);
+        assertTrue(v != VERSION.UNKNOWN, "Unknown password");
+        switch (v.getVersion()) {
+            case (byte) 0:
+                return (passwordHash.equals(getV0Hash(u.getId(), clearpass)));
+            case (byte) 1:
+                byte[] b = Base64.decode(passwordHash);
+                byte[] salt = new byte[VERSION.ONE.getSalt().length];
+                byte[] clientSalt = new byte[VERSION.ONE.getSalt().length];
+                System.arraycopy (b, 1, salt, 0, salt.length);
+                System.arraycopy(b, 1 + salt.length, clientSalt, 0, clientSalt.length);
+                clearpass = clearpass.startsWith("v1:") ? clearpass.substring(3) : upgradeClearPassword(clearpass, clientSalt);
+                String computedPasswordHash = genV1Hash(clearpass, VERSION.ONE.getSalt(salt), clientSalt);
+                return computedPasswordHash.equals(u.getPassword());
+        }
+        return false;
     }
+
+    public boolean upgradePassword (User u, String clearpass)
+            throws HibernateException, BLException
+    {
+        assertNotNull(clearpass, "Invalid pass");
+        String passwordHash = u.getPassword();
+        assertNotNull(passwordHash, "Password is null");
+        VERSION v = VERSION.getVersion(passwordHash);
+        if (v == VERSION.ZERO && passwordHash.equals(getV0Hash(u.getId(), clearpass))) {
+            setV1Password(u, clearpass);
+            return true;
+        }
+        return false;
+    }
+
+    private void setV0Password (User u, String clearpass) {
+        u.setPassword (getV0Hash (u.getId(), clearpass));
+    }
+    private void setV1Password (User u, String clearpass) throws BLException {
+        assertNotNull(clearpass, "Invalid password");
+        byte[] clientSalt = genSalt(VERSION.ONE.getSalt().length);
+        byte[] serverSalt = genSalt(VERSION.ONE.getSalt().length);
+        clearpass = clearpass.startsWith("v1:") ? clearpass : upgradeClearPassword(clearpass, clientSalt);
+        u.setPassword(genV1Hash(clearpass, serverSalt, clientSalt));
+    }
+
     /**
      * @param u the user
      * @param clearpass new password in clear
      * @return true if password is in PasswordHistory
      */
     public boolean checkNewPassword (User u, String clearpass) {
-        String newHash = getHash(u.getId(), clearpass);
+        // TODO - we need to reuse client hash in order to check duplicates
+        String newHash = getV0Hash(u.getId(), clearpass);
         if (newHash.equals(u.getPassword()))
             return false;
         for (PasswordHistory p : u.getPasswordhistory()) {
@@ -143,31 +163,30 @@ public class UserManager {
         return true;
     }
 
-    public void setPassword (User u, String clearpass){
+    public void setPassword(User u, String clearpass) throws BLException {
         setPassword(u, clearpass, null);
     }
 
-    public void setPassword (User u, String clearpass, User author){
+    public void setPassword (User u, String clearpass, User author) throws BLException {
         if (u.getPassword() != null)
             u.addPasswordHistoryValue(u.getPassword());
-        u.setPassword (getHash (u.getId(), clearpass));
+        switch (version) {
+            case ZERO:
+                setV0Password (u, clearpass);
+                break;
+            case ONE:
+                setV1Password (u, clearpass);
+                break;
+
+        }
         RevisionManager revmgr = new RevisionManager(db);
         if (author == null)
             author = u;
         revmgr.createRevision (author, "user." + u.getId(), "Password changed");
-        session.saveOrUpdate(u);
-    }
-    /**
-     * @return all users
-     * @throws HibernateException on low level hibernate related exception
-     */
-    public List findAll () throws HibernateException {
-        return session.createCriteria (User.class)
-                .add (Restrictions.eq ("deleted", Boolean.FALSE))
-                .list();
+        db.session().saveOrUpdate(u);
     }
 
-    public static String getHash (long id, String pass) {
+    private static String getV0Hash (long id, String pass) {
         String hash = null;
         try {
             MessageDigest md = MessageDigest.getInstance ("SHA");
@@ -175,16 +194,6 @@ public class UserManager {
             hash = ISOUtil.hexString (
                 md.digest (pass.getBytes())
             ).toLowerCase();
-        } catch (NoSuchAlgorithmException e) {
-            // should never happen
-        }
-        return hash;
-    }
-    public static String getHash (String s) {
-        String hash = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance ("SHA");
-            hash = ISOUtil.hexString (md.digest (s.getBytes())).toLowerCase();
         } catch (NoSuchAlgorithmException e) {
             // should never happen
         }
@@ -200,5 +209,108 @@ public class UserManager {
         if (!condition)
             throw new BLException (error);
     }
-}
 
+    private String upgradeClearPassword (String clearpass, byte[] salt) {
+        return ISOUtil.hexString(genV1ClientHash(salt, clearpass));
+    }
+
+    private byte[] genSalt(int l) {
+        SecureRandom sr;
+        try {
+            sr = SecureRandom.getInstance("SHA1PRNG");
+            byte[] salt = new byte[l];
+            sr.nextBytes(salt);
+            return salt;
+        } catch (NoSuchAlgorithmException ignored) {
+            // Should never happen, SHA1PRNG is a supported algorithm
+        }
+        return null;
+    }
+
+    private String genV1Hash(String password, byte[] salt, byte[] clientSalt) throws BLException {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            int iterations = VERSION.ONE.getIterations();
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, 2048);
+            return org.bouncycastle.util.encoders.Base64.toBase64String(
+                    Arrays.concatenate(
+                            new byte[]{VERSION.ONE.getVersion()},
+                            VERSION.ONE.getSalt(salt),
+                            clientSalt,
+                            skf.generateSecret(spec).getEncoded())
+            );
+        } catch (Exception e) {
+            throw new BLException (e.getLocalizedMessage());
+        }
+    }
+
+    private byte[] genV1ClientHash (byte[] seed, String clearpass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            int iterations = VERSION.ONE.getIterations();
+            byte[] passAsBytes = clearpass.getBytes(StandardCharsets.UTF_8);
+            for (int i=0; i<iterations; i++) {
+                if (i % 7 == 0)
+                    md.update(seed);
+                md.update(passAsBytes);
+            }
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            // should never happen, SHA-256 is a supported algorithm
+        }
+        return null;
+    }
+
+    public enum VERSION {
+        UNKNOWN((byte)0xFF, 0, 0, 0, null),
+        ZERO((byte)0, 0, 160, 40, null),
+        ONE((byte) 1,100000,2048, 388, Base64.decode("K7f2dgQQHK5CW6Wz+CscUA=="));
+
+        private byte version;
+        private int iterations;
+        private int keylength;
+        private int encodedLength;
+        private byte[] salt;
+
+        VERSION(byte version, int iterations, int keylength, int encodedLength, byte[] salt) {
+            this.version = version;
+            this.iterations = iterations;
+            this.keylength = keylength;
+            this.encodedLength = encodedLength;
+            this.salt = salt;
+        }
+
+        public byte getVersion() {
+            return version;
+        }
+        public int getIterations() {
+            return iterations;
+        }
+        public int getKeylength() {
+            return keylength;
+        }
+
+        public int getEncodedLength() {
+            return encodedLength;
+        }
+
+        public byte[] getSalt() {
+            return ISOUtil.xor(salt, SystemSeed.getSeed(salt.length, salt.length));
+        }
+        public byte[] getSalt(byte[] salt) {
+            return ISOUtil.xor(salt, getSalt());
+        }
+        public static VERSION getVersion (String hash) {
+            for (VERSION v : VERSION.values()) {
+                if (v.getEncodedLength() == hash.length())
+                    return v;
+                if (v != UNKNOWN && v != ZERO) {
+                    byte[] b = Base64.decode(hash);
+                    if (b[0] == v.getVersion())
+                        return v;
+                }
+            }
+            return UNKNOWN;
+        }
+    }
+}
--- modules/eeuser/src/test/java/org/jpos/ee/EEUserTest.java
+++ modules/eeuser/src/test/java/org/jpos/ee/EEUserTest.java
@@ -1,14 +1,15 @@
 package org.jpos.ee;
 
+import org.bouncycastle.util.encoders.Base64;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class EEUserTest {
     DB db;
@@ -20,21 +21,20 @@ public class EEUserTest {
     }
 
     @Test
-    public void tests() {
+    public void tests() throws Exception {
         createUser();
         checkUser();
     }
 
-    private void createUser() {
+    private void createUser() throws BLException {
         db.beginTransaction();
         User user = new User();
-        user.setNick ("admin");
-        user.setName ("User Administrator");
-        user.setActive (true);
+        user.setNick("admin");
+        user.setName("User Administrator");
+        user.setActive(true);
         db.session().save(user);
         UserManager mgr = new UserManager(db);
         mgr.setPassword(user, "test");
-
         Role role = new Role("admin");
         Set<Permission> perms = role.getPermissions();
         perms.add(Permission.valueOf("login"));
@@ -44,17 +44,31 @@ public class EEUserTest {
         user.getRoles().add(role);
         db.commit();
     }
-    public void checkUser() {
+    public void checkUser() throws BLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         db.beginTransaction();
-        UserManager mgr = new UserManager(db);
+        UserManager mgr = new UserManager(db, UserManager.VERSION.ZERO);
         User u = mgr.getUserByNick("admin");
         assertNotNull("User can't be null", u);
         assertTrue("User has 'login' permission", u.hasPermission("login"));
         assertTrue("User has 'admin' permission", u.hasPermission("admin"));
         assertTrue("User has 'admin' role", u.hasPermission("role.admin"));
-        assertTrue("User has all permissions", u.hasAllPermissions(new String[]{"login", "admin", "role.admin" }));
-        assertTrue("User has any permissions", u.hasAnyPermission(new String[]{"nologin", "admin", "role.admin" }));
+        assertTrue("User has all permissions", u.hasAllPermissions(new String[]{"login", "admin", "role.admin"}));
+        assertTrue("User has any permissions", u.hasAnyPermission(new String[]{"nologin", "admin", "role.admin"}));
         assertFalse("User don't have 'superuser' permission", u.hasPermission("superuser"));
+        assertTrue("User password is 'test'", mgr.checkPassword(u, "test"));
+        assertEquals("User hash is correct", "ee89026a6c5603c51b4504d218ac60f6874b7750", u.getPassword());
+        assertTrue("Upgrade password", mgr.upgradePassword(u, "test"));
+        assertNotEquals("User hash has changed", "ee89026a6c5603c51b4504d218ac60f6874b7750", u.getPassword());
+        assertTrue("User upgraded password is still 'test'", mgr.checkPassword(u, "test"));
+
+        byte[] b = Base64.decode(u.getPassword());
+        byte[] clientSalt = new byte[UserManager.VERSION.ONE.getSalt().length];
+        System.arraycopy(b, 1 + clientSalt.length, clientSalt, 0, clientSalt.length);
+
+        Method method = UserManager.class.getDeclaredMethod("upgradeClearPassword", String.class, byte[].class);
+        method.setAccessible(true);
+        String upgradedClearPass = "v1:" + method.invoke(mgr, "test", clientSalt);
+        assertTrue("User upgraded password is still 'hash(test)'", mgr.checkPassword(u, upgradedClearPass));
         db.commit();
     }
 

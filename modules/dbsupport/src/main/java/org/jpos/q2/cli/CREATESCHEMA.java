9f3a8141ef7152927852e546dd9e27e67a9883c6
Alejandro Revilla <apr@jpos.org>
2015-11-16 12:48:55 -0300

    Add CREATESCHEMA CLI command

---
+++ modules/dbsupport/src/main/java/org/jpos/q2/cli/CREATESCHEMA.java
@@ -0,0 +1,35 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.q2.cli;
+
+import org.jpos.q2.CLICommand;
+import org.jpos.q2.CLIContext;
+
+public class CREATESCHEMA implements CLICommand {
+    @Override
+    public void exec(CLIContext cli, String[] args) throws Exception {
+        String filename = null;
+        boolean create = false;
+        if (args.length > 1)
+            filename = args[1];
+        if (args.length > 2)
+            create = "yes".equalsIgnoreCase(args[2]) || "true".equalsIgnoreCase(args[2]);
+        new org.jpos.ee.DB().createSchema(filename, create);
+    }
+}

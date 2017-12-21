30e841bea98c5ff03a02214986334c213e623218
Josefina Revilla <josefinarevilla@gmail.com>
2017-12-21 16:50:26 -0300

    Improved styling of EmptyView

--- modules/qi-core/src/main/java/org/jpos/qi/system/EmptyView.java
+++ modules/qi-core/src/main/java/org/jpos/qi/system/EmptyView.java
@@ -1,5 +1,6 @@
 package org.jpos.qi.system;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.ui.*;
@@ -15,16 +16,19 @@ public class EmptyView extends DefaultView implements ViewDisplay  {
     public EmptyView(boolean canAdd) {
         Label emptyLabel = new Label("There are no items in this view");
         emptyLabel.addStyleName(ValoTheme.LABEL_H2);
-        addComponentsAndExpand(emptyLabel);
+        addComponents(emptyLabel);
         setComponentAlignment(emptyLabel, Alignment.TOP_CENTER);
+        setExpandRatio(emptyLabel,0);
         if (canAdd) {
             Button addNew = new Button(QI.getQI().getMessage("add"));
+            addNew.setIcon(VaadinIcons.PLUS);
             String actualRoute = QI.getQI().getNavigator().getState();
             addNew.addClickListener(listener -> QI.getQI().getNavigator().navigateTo(actualRoute + "/new"));
             addNew.addStyleName(ValoTheme.BUTTON_LARGE);
             addNew.addStyleName(ValoTheme.BUTTON_FRIENDLY);
             addComponent(addNew);
             setComponentAlignment(addNew,Alignment.TOP_CENTER);
+            setExpandRatio(addNew,1);
         }
 
     }

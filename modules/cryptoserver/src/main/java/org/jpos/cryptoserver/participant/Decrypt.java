bb1d58b7f78fe282f95b1b81e999e8c1a3ce7307
Alejandro Revilla <apr@jpos.org>
2018-05-17 23:41:13 -0300

    added cryptoserver module

---
+++ modules/cryptoserver/src/main/java/org/jpos/cryptoserver/participant/Decrypt.java
@@ -0,0 +1,81 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2018 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.cryptoserver.participant;
+
+import java.io.Serializable;
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.crypto.CryptoService;
+import org.jpos.qrest.Mapper;
+import org.jpos.qrest.Response;
+import org.jpos.transaction.Context;
+import org.jpos.transaction.TransactionParticipant;
+import org.jpos.util.NameRegistrar;
+
+import static org.jpos.qrest.Constants.REQUEST;
+import static org.jpos.qrest.Constants.RESPONSE;
+
+public class Decrypt implements TransactionParticipant, Configurable {
+    private CryptoService cs;
+    private static ObjectMapper mapper = Mapper.getMapper();
+    private static Base64.Decoder decoder = Base64.getDecoder();
+    private static UUID jobId = UUID.randomUUID();
+
+    @Override
+    public int prepare(long id, Serializable context) {
+        Context ctx = (Context) context;
+        if (cs.isLocked()) {
+            ctx.put (RESPONSE, new Response(HttpResponseStatus.SERVICE_UNAVAILABLE, "Service unavailable - unlock required"));
+            return FAIL;
+        }
+
+        FullHttpRequest request = ctx.get(REQUEST);
+        try {
+            String jsonStr = request.content().toString(CharsetUtil.UTF_8);
+            ctx.log (jsonStr);
+            @SuppressWarnings("unchecked")
+            Map<String,String> m = (Map<String,String>) mapper.readValue(jsonStr, Map.class);
+            UUID keyId = UUID.fromString(m.get("kid"));
+            byte[] encoded = decoder.decode(m.get("cryptogram"));
+            byte[] b = cs.aesDecrypt(jobId, keyId, encoded);
+            Map<String, Object> r = new LinkedHashMap<>();
+            r.put ("text", new String(b));
+            Response response = new Response(HttpResponseStatus.OK, r);
+            ctx.put(RESPONSE, response);
+        } catch (Exception e) {
+            ctx.log(e);
+            return FAIL;
+        }
+        return PREPARED | NO_JOIN | READONLY;
+    }
+
+    public void setConfiguration (Configuration cfg) throws ConfigurationException {
+        try {
+            cs = NameRegistrar.get(cfg.get("crypto-service", "crypto-service"));
+        } catch (NameRegistrar.NotFoundException e) {
+            throw new ConfigurationException(e.getMessage());
+        }
+    }
+}

1a8b27d7b60a4d634ca14d341b23276d0f0766e3
Alejandro Revilla <apr@jpos.org>
2019-02-04 11:00:23 -0300

    Add MarkdownView component

---
+++ modules/testbed/src/main/java/org/jpos/testbed/MarkdownLorem.java
@@ -0,0 +1,49 @@
+package org.jpos.testbed;
+
+public class MarkdownLorem {
+
+    @Override
+    public String toString() {
+        return 
+            "# Inhospita mortales veteris tenus\n"+
+            "\n"+
+            "## Dixi morabar at cera omnia collis contigerant\n"+
+            "\n"+
+            "Lorem markdownum simulacra deae novorum, *non re ardor* vespere. Magnetas\n"+
+            "laqueosque admotas et restat prensis: addit mersura: circum falsi regia\n"+
+            "verecundo Iovis. Quaerensque ore praemonuisse rura tollere absensauxilium\n"+
+            "sistere *remittis* regem, margine quam aut.\n"+
+            "\n"+
+            "    compactInstallScareware(bus + gigabyte_error_rpc, ad + disk_firewall *\n"+
+            "            processor, definition_bluetooth_ipad);\n"+
+            "    primary -= parallel;\n"+
+            "    var partitionJsonProtector = barebonesWeb.phreakingDns(bootDvdExport);\n"+
+            "\n"+
+            "Contra ignorans fugerent totiens *fixa*. Reclusit vibrata tutos pondere tutus ab\n"+
+            "semper, fulvas, facto tibi latens morte certis, contra, *intrat fluctibus*.\n"+
+            "Crevit Eurylochumque iacent, et nitente simillimus, laboribus per. Et **potentum\n"+
+            "lateo iura** durum deum moenia? Cantusque arboreo, contrahitur micant *inania*.\n"+
+            "\n"+
+            "## Rupta abstulerit furtique posuit vetitorum repperit enim\n"+
+            "\n"+
+            "Secura fratresque decuit, duro Phoebi, idem quod. **Agros satus** adgrediar\n"+
+            "malorum, dicta laboribus cortice viri mora *capillis lumen* domo fertilitas.\n"+
+            "Cunarum viderunt eunt; tamen et Pallas quaeque greges genus ferebant, pontum hi\n"+
+            "ignotas. Pretium mortis miseri refugitque fletus ubi; manus en illis proceres\n"+
+            "enim et nisi libratum.\n"+
+            "\n"+
+            "- Furtim ignota fuerat si comitum dictas\n"+
+            "- Quam metu dies\n"+
+            "- Cum stare altum errat locat praebere sublimia\n"+
+            "- Facto venisset finitque flebant lumen monstri\n"+
+            "- Est varios nos impediebat erat lignum in\n"+
+            "- Inmotosque condeturque planguntur honore\n"+
+            "\n"+
+            "Nostro avumque patulis. Remis inmeritam aliqua; est ait quod *alii sed hunc* sic\n"+
+            "nunc est pectus armis. **Solvitur quae**.\n"+
+            "\n"+
+            "Simul *lucoque* bracchia furentibus et ipsos inplerat. Quam venerat citis\n"+
+            "pedibusque petit, discordibus mixta retroque.\n";
+    }
+}
+

c5ff5adb9bd867a7a51966d9f722557efb6e4a26
Santiago Revilla <spr@jpos.org>
2019-07-05 17:00:03 -0300

    Add dateformat renderer (from messages.properties)

--- modules/qi-core/src/main/java/org/jpos/qi/util/QIUtils.java
+++ modules/qi-core/src/main/java/org/jpos/qi/util/QIUtils.java
@@ -58,4 +58,9 @@ public class QIUtils {
         DateFormat dateFormat     = new SimpleDateFormat(QI.getQI().getMessage("timestampformat"));
         return new DateRenderer(dateFormat);
     }
+
+    public static Renderer createDateRenderer () {
+        DateFormat dateFormat     = new SimpleDateFormat(QI.getQI().getMessage("dateformat"));
+        return new DateRenderer(dateFormat);
+    }
 }

d9ac95a4a00a678af1ce32eca60b0e4331fbc88d
Santiago Revilla <spr@jpos.org>
2020-01-30 11:59:32 -0300

    SysConfigComboBox can show id + value

--- modules/qi-sysconfig/src/main/java/org/jpos/qi/sysconfig/SysConfigComboBox.java
+++ modules/qi-sysconfig/src/main/java/org/jpos/qi/sysconfig/SysConfigComboBox.java
@@ -45,13 +45,26 @@ public class SysConfigComboBox extends ComboBox {
     }
 
     public SysConfigComboBox (String caption, String prefix) throws Exception {
+        this(caption, prefix, false);
+    }
+
+    public SysConfigComboBox (String caption, String prefix, boolean includeId) throws Exception {
         super(caption);
         List items = DB.exec(db -> {
             SysConfigManager mgr = new SysConfigManager(db, prefix);
             return mgr.getAllByValue();
         });
         setItems(items);
-        setItemCaptionGenerator((ItemCaptionGenerator<SysConfig>) SysConfig::getValue);
+        setItemCaptionGenerator((ItemCaptionGenerator<SysConfig>) item -> {
+            if (includeId)
+                return stripPrefixFromId(prefix, item.getId()) + " - " + item.getValue();
+            else
+                return item.getValue();
+        });
         setEmptySelectionAllowed(false);
+    }
+
+    private String stripPrefixFromId (String prefix, String id) {
+        return prefix != null ? id.substring(prefix.length()) : id;
     }
 }

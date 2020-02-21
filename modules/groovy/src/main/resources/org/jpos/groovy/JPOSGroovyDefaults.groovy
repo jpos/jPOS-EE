39430f4156c13316700cc628be174c265c9e13a2
Barzilai Spinak <barspi@transactility.com>
2020-02-21 03:50:29 -0300

    groovy: provide metamethod equivalents of Caller#info

--- modules/groovy/src/main/resources/org/jpos/groovy/JPOSGroovyDefaults.groovy
+++ modules/groovy/src/main/resources/org/jpos/groovy/JPOSGroovyDefaults.groovy
@@ -18,10 +18,14 @@
 
 package org.jpos.groovy
 
-import org.jpos.transaction.*
+import groovy.transform.CompileStatic
+import groovy.transform.Field
 
-// Groovy initialization to spice up Context object
+import org.jpos.transaction.Context
+import org.jpos.util.Caller
 
+
+// ### Groovy initialization to spice up Context object
 
 def ctxmc= Context.metaClass
 
@@ -42,4 +46,61 @@ ctxmc.putAt= { Object key, Object val ->
     delegate.put(key, val)
 }
 
+
+// ### Groovy alternatives for org.jpos.util.Caller#info() methods
+
+
+// We want to ignore stack elements belonging to these classes
+// This list and the shouldExclude method are inspired by org.codehaus.groovy.runtime.StackTraceUtils.isApplicationClass()
+@Field static final List filter= [
+                "groovy.lang.", "org.codehaus.groovy.", "org.apache.groovy.", "gjdk.groovy.",
+                "sun.reflect.", "java.lang.reflect.",
+                "org.jpos.groovy.JPOSGroovyDefaults." ]
+@CompileStatic
+private static boolean shouldExclude(String className)
+{
+  for (String f in filter)
+  {
+    if (className.startsWith(f))
+        return true
+  }
+  return false
+}
+
+
+/* Based on org.jpos.util.Caller.info(int) */
+@CompileStatic
+private static String info(int pos)
+{
+    StackTraceElement [] stels= Thread.currentThread().getStackTrace()
+    StackTraceElement st= stels[stels.length - 1]       // avoid jumping out of the stack
+
+    // Find the useful stack position,  when ignoring internal groovy and java reflection stuff
+    int wantedPos= pos + 3
+    pos= 0
+    for (StackTraceElement el : stels)
+    {
+        if (!shouldExclude(el.getClassName()))
+        {
+            if (pos++ == wantedPos)
+            {
+                st= el  // found it!
+                break
+            }
+        }
+    }
+
+    String clazz = st.getClassName()
+    StringBuilder sb = new StringBuilder(Caller.shortClassName(clazz))
+    return sb.append(".").append(st.getMethodName())
+            .append(':')
+            .append(Integer.toString(st.getLineNumber()))
+            .toString()
+}
+
+
+def callermc= Caller.metaClass
+
+callermc.static.info = { int p -> info(p) }
+callermc.static.info = {       -> info(0) }
 

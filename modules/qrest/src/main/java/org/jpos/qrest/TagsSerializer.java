d7c878e14604d198b0c12045161234eb0b4dae7a
Alejandro Revilla <apr@jpos.org>
2021-12-24 17:51:14 -0300

    Add TagsSerializer

---
+++ modules/qrest/src/main/java/org/jpos/qrest/TagsSerializer.java
@@ -0,0 +1,33 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2021 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.qrest;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.jpos.util.Tags;
+
+import java.io.IOException;
+
+public class TagsSerializer extends JsonSerializer<Tags> {
+    @Override
+    public void serialize(Tags tags, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(tags.toString());
+    }
+}

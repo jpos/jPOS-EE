1f7399f0d6d60b68d510ee02e08e96f0aa574ecf
Bojan Momcilov <bojan.momcilov@yandex.com>
2022-11-28 08:45:40 +0100

    Configuration updates, licences

--- modules/metrics/src/main/java/org/jpos/ee/metrics/Metrics.java
+++ modules/metrics/src/main/java/org/jpos/ee/metrics/Metrics.java
@@ -1,3 +1,20 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2021 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.jpos.ee.metrics;
 
 import com.codahale.metrics.MetricRegistry;
@@ -32,6 +49,7 @@ public class Metrics extends QBeanSupport implements XmlConfigurable
             registry = new MetricRegistry();
             setup(registry);
             SharedMetricRegistries.add(getName(), registry);
+
             if (defaultRegistry)
             {
                 SharedMetricRegistries.setDefault(getName());
@@ -51,7 +69,8 @@ public class Metrics extends QBeanSupport implements XmlConfigurable
         }
     }
 
-    protected void setup(MetricRegistry metrics) {
+    protected void setup(MetricRegistry metrics)
+    {
         metrics.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
         metrics.register("jvm.filedescriptor", new FileDescriptorRatioGauge());
         metrics.register("jvm.classloader", new ClassLoadingGaugeSet());
@@ -88,7 +107,7 @@ public class Metrics extends QBeanSupport implements XmlConfigurable
 
         defaultRegistry = Boolean.parseBoolean(element.getAttributeValue("default", "true"));
 
-        for (Element e : element.getChildren("reporter"))
+        for (Element e : element.getChildren("reporter-factory"))
         {
             String className = e.getAttributeValue("class");
             try
--- modules/metrics/src/main/java/org/jpos/ee/metrics/Q2LogReporterFactory.java
+++ modules/metrics/src/main/java/org/jpos/ee/metrics/Q2LogReporterFactory.java
@@ -1,3 +1,20 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2021 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.jpos.ee.metrics;
 
 import com.codahale.metrics.MetricAttribute;
@@ -43,34 +60,34 @@ public class Q2LogReporterFactory implements MetricsReporterFactory
     {
         requireNonNull(elem);
 
-        String frequency = elem.getChildText("outputFrequency");
+        String frequency = elem.getChildText("output-frequency");
         if (frequency != null)
         {
             outputFrequency = Long.parseLong(frequency);
         }
-        String frequencyUnit = elem.getChildText("outputFrequencyUnit");
+        String frequencyUnit = elem.getChildText("output-frequency-unit");
         if (frequencyUnit != null)
         {
             outputFrequencyUnit = TimeUnit.valueOf(frequencyUnit);
         }
-        String ratesUnit = elem.getChildText("ratesUnit");
+        String ratesUnit = elem.getChildText("rates-unit");
         if (ratesUnit != null)
         {
             rateUnit = TimeUnit.valueOf(ratesUnit);
         }
-        String durationsUnit = elem.getChildText("durationsUnit");
+        String durationsUnit = elem.getChildText("durations-unit");
         if (durationsUnit != null)
         {
             durationUnit = TimeUnit.valueOf(durationsUnit);
         }
-        for (Element exclusion : elem.getChildren("disabledMetricAttributes"))
+        for (Element exclusion : elem.getChildren("exclude-attributes"))
         {
             try
             {
                 String text = exclusion.getTextNormalize();
                 if (!text.isEmpty())
                 {
-                    disabledMetricAttributes.add(MetricAttribute.valueOf(text));
+                    disabledMetricAttributes.add(MetricAttribute.valueOf(text.toUpperCase()));
                 }
             }
             catch (Exception ignore)

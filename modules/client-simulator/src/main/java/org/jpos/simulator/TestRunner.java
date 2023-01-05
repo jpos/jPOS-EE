6b4b9c3343253b72cf36c3641a00e6e5a8b48b6d
Andrés Alcarraz <alcarraz@gmail.com>
2023-01-05 07:17:20 -0500

    No need for `CountdownLatch`.
    Forgot to remove import in last commit.

--- modules/client-simulator/src/main/java/org/jpos/simulator/TestRunner.java
+++ modules/client-simulator/src/main/java/org/jpos/simulator/TestRunner.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;

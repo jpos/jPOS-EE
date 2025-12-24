5f098716f4b670e8986834a5af9fe8733a3e3dba
Alejandro Revilla <apr@jpos.org>
2025-12-23 22:14:20 -0300

    reenable Groovy module

--- modules/groovy/src/main/java/org/jpos/q2/cli/GROOVY.java
+++ modules/groovy/src/main/java/org/jpos/q2/cli/GROOVY.java
@@ -32,57 +32,37 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 
 
-public class GROOVY implements CLICommand
-{
-  @Override
-  public void exec(CLIContext cli, String[] strings) throws Exception
-  {
+public class GROOVY implements CLICommand {
+    @Override
+    public void exec(CLIContext cli, String[] strings) throws Exception {
+       Terminal term = cli.getReader().getTerminal();
 
+        Attributes termAttrs = term.getAttributes();
+        termAttrs.setLocalFlag(Attributes.LocalFlag.ECHO, false);
+        term.setAttributes(termAttrs);
 
-    Terminal term= cli.getReader().getTerminal();
+        InputStream in = term.input();
+        OutputStream out = term.output();
+        OutputStream err = out;
 
-    // Starting with jLine 3.3.0 they changed the defaults for the type of Terminal created
-    // when accessing Q2 --ssh, enabling local echo (which gives duplicated chars on the screen).
-    // Disabling local echo doesn't hurt the type of Terminal created when calling Q2 with the --cli option because
-    // that Terminal already has it disabled
-    Attributes termAttrs= term.getAttributes();
-    termAttrs.setLocalFlag(Attributes.LocalFlag.ECHO, false);
-    term.setAttributes(termAttrs);
+        Binding binding = new Binding();
+        binding.setVariable("q2", cli.getCLI().getQ2());
+        binding.setVariable("cli", cli.getCLI());
+        binding.setVariable("out", new PrintStream(out));  // PrintStream "out" in the Binding will redirect println()
+        binding.setVariable("err", new PrintStream(err));
 
-    // if we use cli.getXxxStream(), it doesn't work correctly during remote access
-    InputStream  in=  term.input();
-    OutputStream out= term.output();
-    OutputStream err= out;
+        IO io = new IO(in, out, err);
 
-    Binding binding= new Binding();
-    binding.setVariable("q2",   cli.getCLI().getQ2());
-    binding.setVariable("cli",  cli.getCLI());
-    binding.setVariable("out",  new PrintStream(out));  // PrintStream "out" in the Binding will redirect println()
-    binding.setVariable("err",  new PrintStream(err));
+        String wasHeadless = System.getProperty("java.awt.headless");
+        System.setProperty("java.awt.headless", "true");    // don't start awt/swing
 
-    IO io= new IO(in, out, err);
+        Groovysh ish = new Groovysh(binding, io);
 
-    String wasHeadless= System.getProperty("java.awt.headless");
-    System.setProperty("java.awt.headless", "true");    // don't start awt/swing
+        if (wasHeadless != null)                            // restore headless value
+            System.setProperty("java.awt.headless", wasHeadless);
+        else
+            System.clearProperty("java.awt.headless");
 
-    Groovysh ish= new Groovysh(binding, io);
-
-    if (wasHeadless != null)                            // restore headless value
-      System.setProperty("java.awt.headless", wasHeadless);
-    else
-      System.clearProperty("java.awt.headless");
-
-    // control will return to exec after groovy session ends
-    ish.run("");
-
-    // append current session's history to $HOME/.groovy/groovysh.history
-    //
-    // TODO: BBB the history file name is currently hardcoded in Groovysh#run(String),
-    // therefore it's shared by all sessions of interactive groovysh for the user.
-    // It would be nice to have it configurable for jPOS purposes, or even per-Q2 app
-    // One option is to subclass Groovysh and @Override run()
-    // Another option is to fork groovy-core, do a proper enhancement, and send a Pull Request
-    ish.getHistory().flush();
-  }
-
+        ish.run("");
+    }
 }
